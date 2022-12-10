8510ad41b79bf602d80abf38f8c572073f124407
ConnorsApps <ConnorSkees@icloud.com>
2022-12-10 14:24:55 -0500

    Up version

--- build-dockerfile.sh
+++ build-dockerfile.sh
@@ -5,7 +5,7 @@ docker buildx build . \
     --build-arg REACT_APP_SNAPCAST_HOST="ws://192.168.1.19:1780" \
     --platform linux/arm64,linux/amd64 \
     --tag connorsapps/snapcast-ui:latest \
-    --tag connorsapps/snapcast-ui:1.0.1 \
+    --tag connorsapps/snapcast-ui:1.0.2 \
     --output type=registry
 
 docker buildx rm snapcast-ui
