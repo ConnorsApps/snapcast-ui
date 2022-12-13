5c271de8ccb3d189095b6733c74a89eaa39d2249
ConnorsApps <ConnorSkees@icloud.com>
2022-12-12 19:43:38 -0500

    Improve how websockets are setup

--- src/utils/AppContext.js
+++ src/utils/AppContext.js
@@ -1,35 +1,20 @@
 import { createContext, useCallback, useEffect, useReducer, useState } from 'react';
 import { EVENTS, REQUESTS } from './Constants.js';
 import { clientsReducer, groupsReducer, stateFromStatus, streamsReducer } from './Reducer.js';
-import { ws, requests, sendRequest, createNewWebsocket, getWebsocketStatus, WEBSOCKET_STATUS } from './WebSocket.js';
-
-const waitForWebsocket = (ws, callback, iterations) => {
-    if (iterations > 10) {
-        return callback({ error: true, message: 'Unable to connect to server' });
-    }
-    setTimeout(() => {
-        if (ws.readyState === 1) {
-            callback({ error: false });
-        } else {
-            waitForWebsocket(ws, callback, ++iterations);
-        }
-    }, 500)
-}
+import { requests, connectToSnapcastServer, WEBSOCKET_STATUS, sendRequest } from './WebSocket.js';
 
 export const AppContext = createContext(null);
 
 export const AppContextProvider = ({ children }) => {
     const [isLoading, setIsLoading] = useState(true);
-    const [init, setInit] = useState(false);
-    const [failed, setFailed] = useState(false);
-    const [webSocketStatus, setWebSocketStatus] = useState(WEBSOCKET_STATUS.connecting);
+    const [status, setStatus] = useState(WEBSOCKET_STATUS.connecting);
 
     const [server, setServer] = useState([]);
     const [groups, disbatchGroups] = useReducer(groupsReducer, {});
     const [streams, disbatchStreams] = useReducer(streamsReducer, {});
     const [clients, disbatchClients] = useReducer(clientsReducer, {});
 
-    const onServerRequest = useCallback((message) => {
+    const onMessage = useCallback((message) => {
         const data = JSON.parse(message.data);
         const isRequest = data.id !== undefined;
 
@@ -61,47 +46,29 @@ export const AppContextProvider = ({ children }) => {
     }, [disbatchStreams, disbatchGroups, disbatchClients, setIsLoading]);
 
     useEffect(() => {
-        setInit(true);
-        // Work around to connect to websocket with hooks
-        if (!init) {
-            waitForWebsocket(ws, ({ error, message }) => {
-                if (!error) {
-                    setWebSocketStatus(WEBSOCKET_STATUS.open);
-                    sendRequest(REQUESTS.server.getStatus, null, true);
-                    ws.addEventListener('message', onServerRequest);
-                } else {
-                    console.error(message);
-                    setIsLoading(false);
-                    setFailed(true);
-                }
-            }, 0);
+        console.log('On status change',status);
+        
+        if (status === WEBSOCKET_STATUS.open) {
+            sendRequest(REQUESTS.server.getStatus, null, true);
         }
+    }, [status]);
 
-    }, [init, onServerRequest]);
+    // useEffect(() => {
+    //     const interval = setInterval(() => setWebSocketStatus(getWebsocketStatus()), 3000);
+
+    //     // Refresh current state and create new websocket when browser comes back into focus
+    //     window.addEventListener('focus', () => {
+    //         createNewWebsocket();
+    //         setInit(false);
+    //         setIsLoading(true);
+    //     });
+
+    //     return () => clearInterval(interval);
+    // }, []);
 
     useEffect(() => {
-        const interval = setInterval(() => setWebSocketStatus(getWebsocketStatus()), 3000);
-
-        // Refresh current state and create new websocket when browser comes back into focus
-        window.addEventListener('focus', () => {
-            createNewWebsocket();
-            setInit(false);
-            setIsLoading(true);
-        });
-
-        return () => clearInterval(interval);
-    }, []);
-
-    useEffect(()=>{
-        const createNewSocket = webSocketStatus === WEBSOCKET_STATUS.closed && init === true;
-
-        if (createNewSocket) {
-            createNewWebsocket();
-        }
-        setInit(createNewSocket ? false : init);
-        setIsLoading(createNewSocket ? true : isLoading);
-
-    }, [webSocketStatus, init, isLoading]);
+        connectToSnapcastServer(0, onMessage, setStatus)
+    }, [onMessage, setStatus]);
 
     return (
         <AppContext.Provider
@@ -114,8 +81,7 @@ export const AppContextProvider = ({ children }) => {
                 disbatchStreams,
                 disbatchGroups,
                 disbatchClients,
-                webSocketStatus,
-                failed,
+                webSocketStatus: status
             }}
         >
             {children}
--- src/utils/WebSocket.js
+++ src/utils/WebSocket.js
@@ -1,9 +1,75 @@
 
 const host = process.env.REACT_APP_SNAPCAST_HOST;
+let ws;
 
-export const createNewWebsocket = () => ws = new WebSocket(`${host}/jsonrpc`);
+// export const createNewWebsocket = () => ws = new WebSocket(`${host}/jsonrpc`);
 
-export let ws = new WebSocket(`${host}/jsonrpc`);
+// export let ws = new WebSocket(`${host}/jsonrpc`);
+export const WEBSOCKET_STATUS = {
+    connecting: 'connecting',
+    open: 'open',
+    failed: 'failed',
+};
+
+const getWebsocketStatus = (ws) => {
+    const state = ws.readyState;
+    let status;
+
+    if (state === 0) {
+        status = WEBSOCKET_STATUS.connecting;
+    } else if (state === 1) {
+        status = WEBSOCKET_STATUS.open;
+    } else if (state === 2 || state === 3) {
+        status = WEBSOCKET_STATUS.connecting;
+    } else {
+        throw new Error('Unknown Websocket Status', state)
+    }
+    return status;
+}
+
+const MAX_RETRIES = 10;
+const RETRY_WAIT = 5;
+
+export const connectToSnapcastServer = (retries = 0, onMessage, onStatus) => {
+    const url = `${host}/jsonrpc`;
+
+    const retry = () => {
+        if (retries > MAX_RETRIES) {
+            onStatus(WEBSOCKET_STATUS.failed);
+            console.error(`Unable to connect to web socket after ${MAX_RETRIES * RETRY_WAIT} seconds.`);
+        } else {
+            connectToSnapcastServer(retries++, onMessage, onStatus);
+        }
+    }
+
+    console.debug(`Connecting to websocket at ${url}`);
+
+    try {
+        ws = new WebSocket(url);
+    } catch (error) {
+        console.error(`Unable to create Websocket ${JSON.stringify(error)}`);
+        retry();
+    }
+
+    ws.onopen = () => {
+        onStatus(getWebsocketStatus(ws));
+        retries = 0;
+    };
+
+    ws.onerror = (error) => {
+        onStatus(getWebsocketStatus(ws));
+        console.error('Websocket emitted error', error);
+    };
+
+    ws.onmessage = onMessage;
+
+    ws.onclose = (event) => {
+        onStatus(getWebsocketStatus(ws));
+
+        console.log(`Websocket closed, retring in ${RETRY_WAIT} seconds. Event ${JSON.stringify(event)}`);
+        setTimeout(retry, RETRY_WAIT * 1000);
+    };
+}
 
 let requestId = 0;
 export const requests = {};
@@ -15,26 +81,4 @@ export const sendRequest = (method, params, saveRequest) => {
     ws.send(JSON.stringify({ id: requestId, jsonrpc: '2.0', method, params }));
 
     requestId++;
-}
-
-export const WEBSOCKET_STATUS = {
-    connecting: 'connecting',
-    open: 'open',
-    closed: 'closed',
-};
-
-export const getWebsocketStatus = () => {
-    const state = ws.readyState;
-    let status;
-
-    if (state === 0) {
-        status = WEBSOCKET_STATUS.connecting;
-    } else if (state === 1) {
-        status = WEBSOCKET_STATUS.open;
-    } else if (state === 2 || state === 3) {
-        status = WEBSOCKET_STATUS.closed;
-    } else {
-        throw new Error('unknown websocket status', state)
-    }
-    return status;
 }
