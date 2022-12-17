0dbbc7e4fb865df4272310eb5ec28a90b2082436
ConnorsApps <ConnorSkees@icloud.com>
2022-12-17 15:34:48 -0500

    Docker compose clients

--- src/pages/Groups/Groups.js
+++ src/pages/Groups/Groups.js
@@ -1,12 +1,24 @@
+import { Paper } from '@mui/material';
 import { useContext } from 'react';
 import Group from '../../components/Group/Group';
 import { AppContext } from '../../utils/AppContext';
 import './Groups.scss';
 
 const Groups = () => {
-    const { groups } = useContext(AppContext);
+    const { groups, isLoading } = useContext(AppContext);
 
     const groupList = Object.values(groups ?? {});
+
+    if (!isLoading && groupList.length === 0) {
+        return (
+            <div className='groups'>
+                <Paper className='noGroups'>
+                    <p>No Groups Found</p>
+                    <p>Add a snapcast Client to get started.</p>
+                </Paper>
+            </div>
+        );
+    }
 
     return (
         <div className='groups'>
