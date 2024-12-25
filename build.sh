105952141db0db6fc994d06627bc32894ed73e7b
ConnorsApps <ConnorSkees@icloud.com>
2024-12-24 20:13:43 -0500

    Use simple bash script for building

---
+++ build.sh
@@ -0,0 +1,21 @@
+
+REACT_APP_HOME_TITLE="Home Audio"
+REACT_APP_SNAPCAST_HOST="ws://node-a.connorskees.com:1780"
+IMAGE_REGISTRY="registry.connorskees.com/lib/snapcast-listener"
+IMAGE_TAG="1.x.x"
+BUILD_PLATFORMS="linux/arm64,linux/amd64"
+
+docker buildx rm snapcast-ui 2>/dev/null
+
+docker buildx create --use --name snapcast-ui
+
+docker buildx build . \
+	--build-arg "REACT_APP_HOME_TITLE=${REACT_APP_HOME_TITLE}" \
+	--build-arg "REACT_APP_SNAPCAST_HOST=${REACT_APP_SNAPCAST_HOST}" \
+	--platform "${BUILD_PLATFORMS}" \
+	--tag "${IMAGE_REGISTRY}:latest" \
+	--tag "${IMAGE_REGISTRY}:${IMAGE_TAG}" \
+	--output type=registry \
+	--push
+
+docker buildx rm snapcast-ui
